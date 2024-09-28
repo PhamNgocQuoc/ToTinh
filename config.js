9fe90ca6e381ff5a0dee5d27c744c28331c22e94
PhamNgocQuoc <hungnxts2208044@fpt.edu.vn>
2024-09-28 10:02:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em bay nhầm vào anh tớ lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, anh à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại gặp được em <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Hẹn gặp em sớm nhất nhó <3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100048903353346' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
